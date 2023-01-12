9a3e0f71294f63a8387f9d0983b54f02661b3903
bemerguy <heiler.bemerguy@gmail.com>
2023-01-11 22:04:22 -0300

    this was making android turn off display..????

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -453,9 +453,6 @@ static ssize_t store_##file_name					\
 	unsigned int ret;						\
 	struct cpufreq_policy new_policy;				\
 									\
-	if (!strcmp(current->comm, "mpdecision") || !strcmp(current->comm, "init")) \
-		return ret;						\
-	printk("Tuned: %s changing min/max frequency\n", current->comm);\
 	ret = cpufreq_get_policy(&new_policy, policy->cpu);		\
 	if (ret)							\
 		return -EINVAL;						\
