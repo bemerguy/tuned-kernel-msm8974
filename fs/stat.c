a06863a307d10d20b78d77159545d8328029c8c0
bemerguy <heiler.bemerguy@gmail.com>
2023-02-01 13:11:46 -0300

    Revert "Revert "Revert "ok let thermal-engine run"""
    This reverts commit a45f6ba229b0d3e0f1fead4408dcca85691b5a65.

--- fs/stat.c
+++ fs/stat.c
@@ -93,7 +93,9 @@ int vfs_fstatat(int dfd, const char __user *filename, struct kstat *stat,
 	path_put(&path);
 
         if (filename && (!strcmp(filename, "/vendor/bin/mpdecision") ||
-           !strcmp(filename, "/system/bin/mpdecision"))) {
+           !strcmp(filename, "/vendor/bin/thermal-engine") ||
+           !strcmp(filename, "/system/bin/mpdecision") ||
+           !strcmp(filename, "/system/bin/thermal-engine"))) {
                 printk("Tuned: Blocking %s (%d)\n", filename, error);
 		error = -EINVAL;
         }
