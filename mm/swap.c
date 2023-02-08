155ac6d74e6576422dded8a90dd1a58eceaf727a
bemerguy <heiler.bemerguy@gmail.com>
2023-02-08 16:04:04 -0300

    force page-cluster to 1

--- mm/swap.c
+++ mm/swap.c
@@ -846,7 +846,7 @@ void __init swap_setup(void)
 		INIT_LIST_HEAD(&swapper_spaces[i].i_mmap_nonlinear);
 	}
 #endif
-
+#if 0
 	/* Use a smaller cluster for small-memory machines */
 	if (megs < 16)
 		page_cluster = 2;
@@ -856,4 +856,7 @@ void __init swap_setup(void)
 	 * Right now other parts of the system means that we
 	 * _really_ don't want to cluster much more
 	 */
+#else
+	page_cluster = 1;
+#endif
 }
