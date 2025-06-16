a55f6a409ff2e9154c91979203c6d40def3534aa
bemerguy <heiler.bemerguy@gmail.com>
2025-06-16 19:37:24 -0300

    Stop userland from changing read_ahead_kbytes

--- block/blk-sysfs.c
+++ block/blk-sysfs.c
@@ -95,7 +95,7 @@ queue_ra_store(struct request_queue *q, const char *page, size_t count)
 	unsigned long ra_kb;
 	ssize_t ret = queue_var_store(&ra_kb, page, count);
 
-	q->backing_dev_info.ra_pages = ra_kb >> (PAGE_CACHE_SHIFT - 10);
+//	q->backing_dev_info.ra_pages = ra_kb >> (PAGE_CACHE_SHIFT - 10);
 
 	return ret;
 }
